a1e6e7b75f3ea6d66cb43d691549f275feeeb612
frost060 <ajinkya.a.gaikwad@capgemini.com>
2018-09-18 11:31:10 +0530

    Added Cors server side

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const User = require("./models/user");
+const cors = require(cors());
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(passport.session());
 app.use(express.json());
 app.use(morgan("tiny"));
 
+app.use(cors());
 app.use("/", users);
 
 app.listen(8000, err => {
